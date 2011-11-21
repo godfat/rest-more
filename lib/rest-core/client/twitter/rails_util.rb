72b5b589f07489c27f9d4093506273752ea97939
Lin Jen-Shin <godfat@godfat.org>
2011-11-22 06:44:10 +0800

    twitter/rails_util.rb: so now rc_setup_twitter :ensure_authorized works

--- lib/rest-core/client/twitter/rails_util.rb
+++ lib/rest-core/client/twitter/rails_util.rb
@@ -23,7 +23,26 @@ module RestCore::Twitter::RailsUtil
                            :with => :rc_twitter_on_unauthorized)
   end
 
-  def rc_twitter_on_access_token_error error=nil
+  def rc_twitter_setup options={}
+    super
+
+    rc_twitter_check_handler
+    rc_twitter_check_session
+    rc_twitter_check_cookies
+    rc_twitter_check_oauth_verifier
+
+    if rc_options_get(RestCore::Twitter, :ensure_authorized) &&
+       !rc_twitter.authorized?
+
+      rc_twitter_authorize('ensure authorized')
+      false # action halt, redirect to do authorize,
+            # eagerly, as opposed to auto_authorize
+    else
+      true  # keep going
+    end
+  end
+
+  def rc_twitter_on_unauthorized error=nil
     rc_twitter_authorize(error, false)
   end
 
@@ -31,20 +50,112 @@ module RestCore::Twitter::RailsUtil
     logger.warn("WARN: Twitter: #{error.inspect}")
 
     if force_redirect || rc_options_get(RestCore::Twitter, :auto_authorize)
-      redirect_url = rc_twitter.authorize_url!
+      rc_twitter_cleanup
+
+      callback = rc_twitter_normalized_request_uri
+      redirect_url = rc_twitter.authorize_url!(:oauth_callback => callback)
+      rc_twitter.oauth_callback = callback
+      rc_twitter_write_data_json
 
       logger.debug(
-        "DEBUG: Twitter: redirect to #{redirect_url}")
+        "DEBUG: Twitter: redirect to #{redirect_url} from #{callback}")
 
-      rc_twitter_cleanup
       redirect_to redirect_url
     end
   end
 
-  def rc_twitter_cleanup
-    cookies.delete(rc_twitter_storage_key)
-    session.delete(rc_twitter_storage_key)
+  # ==================== begin check ================================
+  def rc_twitter_storage_key
+    "rc_twitter_#{rc_twitter.consumer_key}"
   end
+
+  def rc_twitter_check_oauth_verifier
+    return if !rc_twitter.callback || !params[:oauth_verifier]
+
+    rc_twitter.authorize!(:oauth_verifier => params[:oauth_verifier])
+    rc_twitter.data.delete('callback')
+
+    logger.debug(
+      "DEBUG: Twitter: detected oauth_verifier with #{request.url}," \
+      " parsed: #{rc_twitter.data.inspect}")
+
+    rc_twitter_write_data_json if rc_twitter.authorized?
+  end
+
+  def rc_twitter_check_handler handler=
+      rc_options_get(RestCore::Twitter, :check_handler)
+
+    return if rc_twitter.authorized? || !handler
+    rc_twitter.data_json = handler.call
+    logger.debug("DEBUG: Twitter: called check_handler, parsed:" \
+                 " #{rc_twitter.data.inspect}")
+  end
+
+  def rc_twitter_check_session
+    return if rc_twitter.authorized?                             ||
+              !rc_options_get(RestCore::Twitter, :write_session) ||
+              !(data_json = session[rc_twitter_storage_key])
+
+    rc_twitter.data_json = data_json
+    logger.debug("DEBUG: Twitter: detected rc_twitter session, parsed:" \
+                 " #{rc_twitter.data.inspect}")
+  end
+
+  def rc_twitter_check_cookies
+    return if rc_twitter.authorized?                             ||
+              !rc_options_get(RestCore::Twitter, :write_cookies) ||
+              !(data_json = cookies[rc_twitter_storage_key])
+
+    rc_twitter.data_json = data_json
+    logger.debug("DEBUG: Twitter: detected rc_twitter cookies, parsed:" \
+                 " #{rc_twitter.data.inspect}")
+  end
+  # ====================   end check ================================
+  # ==================== begin write ================================
+  def rc_twitter_write_data_json
+    rc_twitter_write_handler
+    rc_twitter_write_session
+    rc_twitter_write_cookies
+  end
+
+  def rc_twitter_write_handler handler=
+    rc_options_get(RestCore::Twitter, :write_handler)
+
+    return if !handler
+    handler.call(data_json = rc_twitter.data_json)
+    logger.debug("DEBUG: Twitter: write_handler: data_json => #{data_json}")
+  end
+
+  def rc_twitter_write_session
+    return if !rc_options_get(RestCore::Twitter, :write_session)
+    session[rc_twitter_storage_key] = data_json = rc_twitter.data_json
+    logger.debug("DEBUG: Twitter: wrote session: data_json => #{data_json}")
+  end
+
+  def rc_twitter_write_cookies
+    return if !rc_options_get(RestCore::Twitter, :write_cookies)
+    cookies[rc_twitter_storage_key] = data_json = rc_twitter.data_json
+    logger.debug("DEBUG: Twitter: wrote cookies: data_json => #{data_json}")
+  end
+  # ==================== end write ================================
+
+
+
+  # ==================== begin misc ================================
+  def rc_twitter_cleanup
+    session.delete(rc_twitter_storage_key)
+    cookies.delete(rc_twitter_storage_key)
+  end
+
+  def rc_twitter_normalized_request_uri
+    URI.parse(request.url).tap{ |uri|
+      uri.query = uri.query.split('&').reject{ |q|
+                    q =~ /^(oauth_verifier|oauth_token)\=/
+                  }.join('&') if uri.query
+      uri.query = nil if uri.query.blank?
+    }.to_s
+  end
+  # ==================== end misc ================================
 end
 
 RestCore::Twitter::RailsUtil.init(Rails)
