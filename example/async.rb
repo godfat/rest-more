d2855c5945ce5045045f613928e2805888fe426c
Lin Jen-Shin <godfat@godfat.org>
2012-02-22 03:16:27 +0800

    demonstrate how async timeout would work

--- example/async.rb
+++ example/async.rb
@@ -3,17 +3,35 @@ require 'rest-more'
 
 # RC::Builder.default_app = # for global default_app setting
 
+def aget_facebook opts={}
+  RC::Facebook.new(opts).get('4'){ |response|
+    if response.kind_of?(::Timeout::Error)
+      puts "TIMEOUT"
+    else
+      p response
+      puts "DONE"
+    end
+  }
+end
+
 puts "RC::Coolio"
 RC::Facebook.builder.default_app = RC::Coolio
-RC::Facebook.new.get('4'){ |r| p r }
+aget_facebook(:timeout => 0.01)
+aget_facebook(:timeout => 10.0)
 Coolio::Loop.default.run
 puts
 
-
+def get_facebook opts={}
+  p RC::Facebook.new(opts).get('4')
+  puts "DONE"
+rescue Timeout::Error
+  puts "TIMEOUT"
+end
 
 puts "RC::CoolioFiber"
 RC::Facebook.builder.default_app = RC::CoolioFiber
-Fiber.new{ p RC::Facebook.new.get('4'); puts "DONE" }.resume
+Fiber.new{ get_facebook(:timeout => 0.01) }.resume
+Fiber.new{ get_facebook(:timeout => 10.0) }.resume
 Coolio::Loop.default.run
 puts
 
