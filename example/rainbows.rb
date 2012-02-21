59277f951c56dd2c7394fa6651b9de8c955ec5a4
Lin Jen-Shin <godfat@godfat.org>
2012-02-22 06:54:41 +0800

    example/rainbows.rb: you might want this to stop em from crashing...

--- example/rainbows.rb
+++ example/rainbows.rb
@@ -20,3 +20,21 @@ class RainbowsEventMachineFiberClient < Rainbows::EventMachine::Client
     Fiber.new{ super }.resume
   end
 end
+
+# monkey patch eventmachine to ignore errors and report them,
+# instead of crashing!!
+module ::EventMachine
+  class << self
+    alias_method :crashing_stop, :stop
+  end
+
+  def self.stop
+    if @wrapped_exception
+      $stderr.puts("WARN: #{@wrapped_exception.inspect}: " \
+                   "#{@wrapped_exception.backtrace.inspect}")
+      @wrapped_exception = nil
+    else
+      crashing_stop
+    end
+  end
+end
