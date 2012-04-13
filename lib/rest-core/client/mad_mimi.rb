56aac0e0e8485c5063733037588de2775ceada50
Lin Jen-Shin <godfat@godfat.org>
2012-04-14 05:38:23 +0800

    Squashed commit of the following:
    commit 32620c017cc1a8daf3da77081183f3852998ff74
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Wed Nov 30 10:09:54 2011 +0800
    
        use the constants from RestCore
    
    commit ce4746d185f6695c4669e476bfa3a7ce935349b5
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Wed Nov 30 10:07:36 2011 +0800
    
        remove cache.clear
    
    commit 1ced0e702cb16d75b2917e1a3a3f3a2f7f4819a8
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 18:50:41 2011 +0800
    
        don't convert the status to symbol
    
    commit fb488bdef244470db5bd8066044653fa84612818
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 18:46:47 2011 +0800
    
        explicit assign instance variables
    
    commit d2557c82863070e9bca59744172f8680e1498696
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 18:45:53 2011 +0800
    
        always uses self.name
    
    commit 98b377bed67392029215091e5afb1ab924aff766
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 18:27:52 2011 +0800
    
        default site ended with slash
    
    commit 4edda2125458c1a34069436de1e368c675b45f8f
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 17:21:57 2011 +0800
    
        add/remove member in list model
    
    commit ba84fb531db01e3fef52d24de852aa13b8c9b314
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 17:14:15 2011 +0800
    
        add/remove members
    
    commit 64d3f4d893e5b64fb2b6e52c12076a5ed8223509
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 17:06:19 2011 +0800
    
        use built-in error detector and handler middleware
    
    commit e8e38651d2d8b93041bd639e83d8ca27f95d0d8f
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 16:11:17 2011 +0800
    
        sending mails via audience list model
    
    commit 1eeab8decf4e0d0883cd5b73af20d3a87bc1aed6
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 16:07:35 2011 +0800
    
        add test for audience list model
    
    commit e59087ba6aa5b387b2e9a1873701898dacb9d1d6
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 15:52:34 2011 +0800
    
        create, rename, and destroy audience list
    
    commit 79576f5ca49cca3e2209546b7ec43dfdb3a3953c
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 14:39:49 2011 +0800
    
        rename test_api to test_mailer_api
    
    commit 1f1772ac29f573564db5a3a5ee8aa491b4b31ca5
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 14:08:03 2011 +0800
    
        add some tests for MadMimi client
    
    commit 53144c4e3ba1c97b42594059683e4cdac8add5bb
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 13:20:00 2011 +0800
    
        autoload MadMimi
    
    commit ead4aaf376cfbe93a4934905dd8e0c8b17468908
    Author: ayaya <ayaya@ayaya.tw>
    Date:   Tue Nov 29 13:18:21 2011 +0800
    
        implements MadMimi mailer apis

---
+++ lib/rest-core/client/mad_mimi.rb
@@ -0,0 +1,123 @@
+require 'rest-core'
+require 'crack/xml'
+
+RestCore::MadMimi = RestCore::Builder.client(:data, :username, :api_key,
+                                             :promotion_name) do
+  s = self.class # this is only for ruby 1.8!
+  use s::Timeout       , 300
+  use s::DefaultSite   , 'https://api.madmimi.com/'
+  use s::DefaultQuery  , {}
+  use s::Cache         , {}, 60 do
+    use s::ErrorHandler, lambda { |env|
+                           raise RestCore::MadMimi::Error.call(env) }
+    use s::ErrorDetectorHttp
+  end
+  use s::Defaults      , :data => {}
+end
+
+class RestCore::MadMimi::Error < RestCore::Error
+  def self.call(env)
+    raise RestCore::MadMimi::Error,
+      "#{env[RestCore::RESPONSE_STATUS]} #{env[RestCore::RESPONSE_BODY]}"
+  end
+end
+
+require 'rest-core/client/mad_mimi/audience_list'
+
+module RestCore::MadMimi::Client
+  include RestCore
+
+  POSSIBLE_STATUSES = %w[ignorant sending failed sent received clicked_through
+                         bounced retried retry_failed forwarded opted_out
+                         abused]
+
+  # https://madmimi.com/developer/mailer/transactional
+  #
+  # Usage:
+  #
+  #   client.mailer('ayaya@example.com',
+  #                 :subject => 'Subject',
+  #                 :raw_html => 'the mail body [[tracking_beacon]]')
+  #
+  # The transaction code is convert to integer
+  def mailer(recipient, options = {})
+    options = {:recipients => recipient}.merge(options)
+    response = post('mailer', options)
+    # response was a string that included RestClient::AbstractResponse,
+    # and it overrided #to_i method (which returns status code)
+    String.new(response).to_i
+  end
+
+  # https://madmimi.com/developer/mailer/send-to-a-list
+  #
+  # Usage:
+  #
+  #   client.mailer_to_list('list_name',
+  #                         :subject => 'Subject',
+  #                         :raw_html => 'the mail body [[tracking_beacon]]')
+  #
+  # The transaction code is convert to integer
+  def mailer_to_list(list, options = {})
+    list = list.join(',') if list.is_a?(Array)
+    options = {:list_name => list}.merge(options)
+    response = post('mailer/to_list', options)
+    # response was a string that included RestClient::AbstractResponse,
+    # and it overrided #to_i method (which returns status code)
+    String.new(response).to_i
+  end
+
+  # https://madmimi.com/developer/mailer/status
+  #
+  # Usage:
+  #
+  #   id = client.mailer(...)
+  #   client.status(id)
+  def status(id)
+    get("mailers/status/#{id.to_i}")
+  end
+
+  # https://madmimi.com/developer/lists
+  # Audience lists apis
+
+  def audience_lists
+    response = get('audience_lists/lists.xml')
+    Crack::XML.parse(response)['lists']['list'].map do |list|
+      RestCore::MadMimi::AudienceList.new(self, list)
+    end
+  end
+
+  def create_audience_list(name)
+    post('audience_lists', :name => name)
+    RestCore::MadMimi::AudienceList.new(self, :name => name)
+  end
+
+  def rename_audience_list(name, new_name)
+    post("audience_lists/#{CGI.escape(name)}/rename", :name => new_name)
+    RestCore::MadMimi::AudienceList.new(self, :name => new_name)
+  end
+
+  def destroy_audience_list(name)
+    post("audience_lists/#{CGI.escape(name)}", :_method => 'delete')
+  end
+
+  # https://madmimi.com/developer/lists
+  # Audience lists apis (members)
+
+  def add_member_to_audience_list(list, email, options = {})
+    options = {:email => email}.merge(options)
+    post("audience_lists/#{CGI.escape(list)}/add", options)
+  end
+
+  def remove_member_from_audience_list(list, email, options = {})
+    options = {:email => email}.merge(options)
+    post("audience_lists/#{CGI.escape(list)}/remove", options)
+  end
+
+  def query
+    {'username' => username,
+     'api_key' => api_key,
+     'promotion_name' => promotion_name}
+  end
+end
+
+RestCore::MadMimi.send(:include, RestCore::MadMimi::Client)
---
+++ lib/rest-core/client/mad_mimi/audience_list.rb
@@ -0,0 +1,39 @@
+class RestCore::MadMimi::AudienceList
+  attr_reader :client
+  attr_reader :id, :name, :display_name, :subscriber_count
+
+  def initialize(client, values = {})
+    @client = client
+    %w[id name display_name subscriber_count].each do |key|
+      value = values[key] || values[key.to_sym]
+      instance_variable_set("@#{key}", value)
+    end
+  end
+
+  def mailer(options = {})
+    @client.mailer_to_list(self.name, options)
+  end
+
+  def name=(new_name)
+    client.rename_audience_list(self.name, new_name)
+    @name = new_name
+  end
+
+  def destroy
+    client.destroy_audience_list(self.name)
+  end
+
+  def add_member(email)
+    client.add_member_to_audience_list(self.name, email)
+  end
+
+  def remove_member(email)
+    client.remove_member_from_audience_list(self.name, email)
+  end
+
+  def to_s
+    %w"#<RestCore::MadMimi::AudienceList
+      @id=%s @name=%s @display_name=%s @subscriber_count=%s>".
+      join(' ') % [id, name, display_name, subscriber_count].map(&:inspect)
+  end
+end
