146b42853142a91ba324e8092d0c2e5543d00c8c
Lin Jen-Shin <godfat@godfat.org>
2012-07-26 17:13:58 +0800

    remove autoload for mixi and bing

--- lib/rest-more.rb
+++ lib/rest-more.rb
@@ -4,11 +4,9 @@ require 'rest-core'
 module RestCore
   autoload :Config  , 'rest-core/util/config'
 
-  autoload :Bing    , 'rest-core/client/bing'
   autoload :Dropbox , 'rest-core/client/dropbox'
   autoload :Facebook, 'rest-core/client/facebook'
   autoload :Github  , 'rest-core/client/github'
   autoload :Linkedin, 'rest-core/client/linkedin'
-  autoload :Mixi    , 'rest-core/client/mixi'
   autoload :Twitter , 'rest-core/client/twitter'
 end
