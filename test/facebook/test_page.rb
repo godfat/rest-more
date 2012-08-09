7fa8ddd2bba7bc9ec235123e8a7acd8ee90af078
Lin Jen-Shin <godfat@godfat.org>
2012-08-09 09:13:01 +0800

    no more rest-graph, it's simply facebook client

--- test/facebook/test_page.rb
+++ test/facebook/test_page.rb
@@ -8,64 +8,64 @@ describe RC::Facebook do
   end
 
   should 'get the next/prev page' do
-    rg = RC::Facebook.new(:site => '', :cache => false)
+    f = RC::Facebook.new(:site => '', :cache => false)
     %w[next previous].each{ |type|
       kind = "#{type}_page"
-      rg.send(kind, {})              .should.eq nil
-      rg.send(kind, {'paging' => []}).should.eq nil
-      rg.send(kind, {'paging' => {}}).should.eq nil
+      f.send(kind, {})              .should.eq nil
+      f.send(kind, {'paging' => []}).should.eq nil
+      f.send(kind, {'paging' => {}}).should.eq nil
 
       stub_request(:get, 'zzz').to_return(:body => '["ok"]')
-      rg.send(kind, {'paging' => {type => 'zzz'}}).should.eq ['ok']
+      f.send(kind, {'paging' => {type => 'zzz'}}).should.eq ['ok']
     }
   end
 
   should 'merge all pages into one' do
-    rg = RC::Facebook.new(:site => '', :cache => false)
+    f = RC::Facebook.new(:site => '', :cache => false)
     %w[next previous].each{ |type|
       kind = "#{type}_page"
       data = {'paging' => {type => 'zzz'}, 'data' => ['z']}
 
-      rg.for_pages(data, -1, {}, kind).should == nil
-      rg.for_pages(data,  0, {}, kind).should == nil
-      rg.for_pages(data,  1, {}, kind).should == data
+      f.for_pages(data, -1, {}, kind).should == nil
+      f.for_pages(data,  0, {}, kind).should == nil
+      f.for_pages(data,  1, {}, kind).should == data
 
       (2..4).each{ |pages|
         # merge data
         stub_request(:get, 'zzz').to_return(:body => '{"data":["y"]}')
-        rg.for_pages(data, pages, {}, kind).should.eq({'data' => %w[z y]})
+        f.for_pages(data, pages, {}, kind).should.eq({'data' => %w[z y]})
 
         # this data cannot be merged
         stub_request(:get, 'zzz').to_return(:body => '{"data":"y"}')
-        rg.for_pages(data, pages, {}, kind).should.eq({'data' => %w[z]})
+        f.for_pages(data, pages, {}, kind).should.eq({'data' => %w[z]})
       }
 
       stub_request(:get, 'zzz').to_return(:body =>
         '{"paging":{"'+type+'":"yyy"},"data":["y"]}')
       stub_request(:get, 'yyy').to_return(:body => '{"data":["x"]}')
 
-      rg.for_pages(data, 3, {}, kind).should.eq({'data' => %w[z y x]})
+      f.for_pages(data, 3, {}, kind).should.eq({'data' => %w[z y x]})
     }
   end
 
   should 'for_pages with callback' do
-    rg = RC::Facebook.new(:site => '', :cache => false)
+    f = RC::Facebook.new(:site => '', :cache => false)
     %w[next previous].each{ |type|
       kind = "#{type}_page"
       data = {'paging' => {type => 'zzz'}, 'data' => ['z']}
 
       # invalid pages or just the page itself
-      rg.for_pages(data, -1, {}, kind){ |r| r.should.eq nil }.should.eq rg
-      rg.for_pages(data,  0, {}, kind){ |r| r.should.eq nil }.should.eq rg
+      f.for_pages(data, -1, {}, kind){ |r| r.should.eq nil }.should.eq f
+      f.for_pages(data,  0, {}, kind){ |r| r.should.eq nil }.should.eq f
       a = []
-      rg.for_pages(data,  1, {}, kind){ |r| a << r          }.should.eq rg
+      f.for_pages(data,  1, {}, kind){ |r| a << r          }.should.eq f
       a.should.eq [data, nil]
 
       (2..4).each{ |pages|
         # merge data
         stub_request(:get, 'zzz').to_return(:body => '{"data":["y"]}')
         expects = [data, {'data' => %w[y]}, nil]
-        rg.for_pages(data, pages, {}, kind){ |r|
+        f.for_pages(data, pages, {}, kind){ |r|
           r.should.eq expects.shift
         }.wait
         expects.should.empty?
@@ -73,7 +73,7 @@ describe RC::Facebook do
         # this data cannot be merged
         stub_request(:get, 'zzz').to_return(:body => '{"data":"y"}')
         expects = [data, {'data' => 'y'}, nil]
-        rg.for_pages(data, pages, {}, kind){ |r|
+        f.for_pages(data, pages, {}, kind){ |r|
           r.should.eq expects.shift
         }.wait
         expects.should.empty?
@@ -86,7 +86,7 @@ describe RC::Facebook do
       t = Thread.current
       expects = [data, {'data' => %w[y], 'paging' => {type => 'yyy'}},
                        {'data' => %w[x]}, nil]
-      rg.for_pages(data, 3, {}, kind){ |r|
+      f.for_pages(data, 3, {}, kind){ |r|
         r.should.eq expects.shift
         t.wakeup if expects.empty?
       }.wait
