667d04d760d7a0f93204f2845a126cc8036a6f4f
Lin Jen-Shin <godfat@godfat.org>
2012-08-14 07:22:30 +0800

    move TestHelper from rest-core to rest-more

--- lib/rest-more/test.rb
+++ lib/rest-more/test.rb
@@ -1,3 +1,14 @@
 
 require 'rest-core/test'
 require 'rest-more'
+
+module TestHelper
+  module_function
+  def normalize_query query
+    '?' + query[1..-1].split('&').sort.join('&')
+  end
+
+  def normalize_url url
+    url.sub(/\?.+/){ |query| TestHelper.normalize_query(query) }
+  end
+end
