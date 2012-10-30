6daa195fe22cce5649e3f916298977756fd95a3a
Lin Jen-Shin <godfat@godfat.org>
2012-10-31 04:51:07 +0800

    example/rainbows.rb: update from pic-collage

--- example/rainbows.rb
+++ example/rainbows.rb
@@ -4,7 +4,9 @@ preload_app      true
 
 Rainbows! do
   use :EventMachine, :em_client_class => lambda{
-    RainbowsEventMachineFiberClient
+    RainbowsEventMachineThreadPoolClient
+    # or
+    # RainbowsEventMachineFiberSpawnClient
   }
   worker_connections        100
 
@@ -12,11 +14,53 @@ Rainbows! do
   client_header_buffer_size  8*1024      #  8 kilobytes
 end
 
-class RainbowsEventMachineFiberClient < Rainbows::EventMachine::Client
-  def app_call input
-    Fiber.new{ super }.resume
+after_fork{ |_, _|
+  EM.threadpool_size = 32
+  class RainbowsEventMachineThreadPoolClient < Rainbows::EventMachine::Client
+    def app_call input
+      set_comm_inactivity_timeout 0
+      @env[RACK_INPUT] = input
+      @env[REMOTE_ADDR] = @_io.kgio_addr
+      @env[ASYNC_CALLBACK] = method(:write_async_response)
+      @env[ASYNC_CLOSE] = EM::DefaultDeferrable.new
+      @deferred = true
+      EM.defer{
+        status, headers, body = catch(:async) {
+          APP.call(@env.merge!(RACK_DEFAULTS))
+        }
+        if nil == status || -1 == status
+          @deferred = true
+        else
+          @deferred = nil
+          ev_write_response(status, headers, body, @hp.next?)
+        end
+      }
+    end
   end
-end
+
+  class RainbowsEventMachineFiberSpawnClient < Rainbows::EventMachine::Client
+    def app_call input
+      set_comm_inactivity_timeout 0
+      @env[RACK_INPUT] = input
+      @env[REMOTE_ADDR] = @_io.kgio_addr
+      @env[ASYNC_CALLBACK] = method(:write_async_response)
+      @env[ASYNC_CLOSE] = EM::DefaultDeferrable.new
+      f = Fiber.new{
+        status, headers, body = catch(:async) {
+          APP.call(@env.merge!(RACK_DEFAULTS))
+        }
+        if nil == status || -1 == status
+          @deferred = true
+        else
+          @deferred = nil
+          ev_write_response(status, headers, body, @hp.next?)
+        end
+      }
+      f.resume
+      @deferred = true if f.alive?
+    end
+  end
+}
 
 EM.error_handler{ |e|
   puts "Error: EM.error_handler: #{e.inspect} #{e.backtrace.inspect}"
