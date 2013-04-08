53169ab14c0cad470ce4aff4591d75f63bb5e3cd
Lin Jen-Shin <godfat@godfat.org>
2013-04-08 19:37:04 +0800

    twitter/test_api.rb: restore the tests

--- test/twitter/test_api.rb
+++ test/twitter/test_api.rb
@@ -15,24 +15,24 @@ describe RC::Twitter do
     RC::Twitter.new.me.should.eq({'status' => 'OK'})
   end
 
-#  def check status, klass
-#    stub_request(:delete, 'https://api.twitter.com/123').to_return(
-#      :body => '{}', :status => status)
-#
-#    lambda{
-#      RC::Twitter.new.delete('123').tap{}
-#    }.should.raise(klass)
-#
-#    WebMock.reset!
-#  end
-#
-#  should 'raise exception when encountering error' do
-#    [401, 402, 403].each{ |status|
-#      check(status, RestCore::Twitter::Error)
-#    }
-#
-#    [500, 502, 503].each{ |status|
-#      check(status, RestCore::Twitter::Error::ServerError)
-#    }
-#  end
+  def check status, klass
+    stub_request(:delete, 'https://api.twitter.com/123').to_return(
+      :body => '{}', :status => status)
+
+    lambda{
+      RC::Twitter.new.delete('123').tap{}
+    }.should.raise(klass)
+
+    WebMock.reset!
+  end
+
+  should 'raise exception when encountering error' do
+    [401, 402, 403].each{ |status|
+      check(status, RestCore::Twitter::Error)
+    }
+
+    [500, 502, 503].each{ |status|
+      check(status, RestCore::Twitter::Error::ServerError)
+    }
+  end
 end
