e7d548474e42869de34e3fcb8b628c612d5a0d73
Lin Jen-Shin <godfat@godfat.org>
2013-06-04 18:56:26 +0800

    wondering if anyone could fix this inconsistency

--- example/rails3/test/test_helper.rb
+++ example/rails3/test/test_helper.rb
@@ -2,10 +2,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-::Test::Unit::AssertionFailedError = Class.new(RuntimeError)
-require 'rr'
 require 'webmock'
 WebMock.disable_net_connect!
+require 'rr'
+::Test::Unit::AssertionFailedError = Class.new(RuntimeError)
 
 class ActiveSupport::TestCase
   def normalize_query query, amp='&'
