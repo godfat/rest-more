56adf78dd8104ef530f4e62d291fb771342b52c4
Lin Jen-Shin <godfat@godfat.org>
2014-01-07 00:34:14 +0800

    update rest-core and adopt latest muack

--- test/facebook/test_timeout.rb
+++ test/facebook/test_timeout.rb
@@ -11,7 +11,7 @@ describe RC::Facebook do
     stub_request(:get, 'https://graph.facebook.com/me').
       to_return(:body => '{}')
     any_instance_of(RC::Timeout::TimerThread){ |timer|
-      mock(timer).on_timeout.proxy
+      mock(timer).on_timeout
     }
     RC::Facebook.new.get('me').should.eq({})
   end
@@ -19,7 +19,7 @@ describe RC::Facebook do
   should 'override timeout' do
     stub_request(:get, 'https://graph.facebook.com/me').
       to_return(:body => 'true')
-    mock(RC::Timeout::TimerThread).new(99, is_a(Timeout::Error)).proxy
+    mock(RC::Timeout::TimerThread).new(99, is_a(Timeout::Error))
     RC::Facebook.new(:timeout => 1).get('me', {}, :timeout => 99).
       should.eq true
   end
