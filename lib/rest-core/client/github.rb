9d84da44e4c47c61342047bf94ce3a55f8c203af
Lin Jen-Shin <godfat@godfat.org>
2014-06-12 05:49:26 +0800

    implement RC::Github#all

--- lib/rest-core/client/github.rb
+++ lib/rest-core/client/github.rb
@@ -23,8 +23,48 @@ end
 module RestCore::Github::Client
   include RestCore
 
+  MAX_PER_PAGE = 100
+
   def me query={}, opts={}, &cb
     get('user', query, opts, &cb)
+  end
+
+  def all path, query={}, opts={}
+    q = {:per_page => MAX_PER_PAGE}.merge(query)
+    r = get(path, q, opts.merge(RESPONSE_KEY => PROMISE)).then{ |response|
+      body = response[RESPONSE_BODY] + (page_range(response).map{ |page|
+        get(path, q.merge(:page => page),
+            opts.merge(RESPONSE_KEY => RESPONSE_BODY))
+      }.inject(&:+))
+      response.merge(RESPONSE_BODY => body)
+    }.future_response
+
+    if block_given?
+      yield(r[response_key(opts)])
+    else
+      r[response_key(opts)]
+    end
+  end
+
+  private
+  def page_range response
+    from = (parse_current_page(response) || 1).to_i + 1
+    to   = (parse_last_page(response) || from).to_i
+    if from <= to
+      from..to
+    else
+      []
+    end
+  end
+
+  def parse_current_page response
+    RC::ParseQuery.parse_query(URI.parse(response[REQUEST_URI]).query)['page']
+  end
+
+  def parse_last_page response
+    links = RC::ParseLink.parse_link(response[RESPONSE_HEADERS]['LINK'])
+    return unless last_link = links['last']
+    RC::ParseQuery.parse_query(URI.parse(last_link['uri']).query)['page']
   end
 end
 
