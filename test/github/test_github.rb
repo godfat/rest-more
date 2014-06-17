be46e70ce488d669a9c5ad9b795ebe24c4a90b06
Lin Jen-Shin <godfat@godfat.org>
2014-06-17 18:44:27 +0800

    test for github and stackexchange

---
+++ test/github/test_github.rb
@@ -0,0 +1,31 @@
+
+require 'rest-more/test'
+
+describe RC::Github do
+  after do
+    WebMock.reset!
+  end
+
+  should 'get all' do
+    link = '</users/godfat/repos?type=o&per_page=100&page=3>; rel="last"'
+    headers = {'Link' => link}
+    stub_request(:get,
+      'https://api.github.com/users/godfat/repos?type=o&per_page=100').
+      to_return(:body => [0], :headers => headers).times(2)
+    stub_request(:get,
+      'https://api.github.com/users/godfat/repos?type=o&per_page=100&page=2').
+      to_return(:body => [1], :headers => headers).times(2)
+    stub_request(:get,
+      'https://api.github.com/users/godfat/repos?type=o&per_page=100&page=3').
+      to_return(:body => [2], :headers => headers).times(2)
+
+    args = ['users/godfat/repos', {:type => 'o'}]
+    exps = [0, 1, 2]
+    g = RC::Github.new
+    g.all(*args) do |res|
+      res.should.eq exps
+      g.all(*args).should.eq exps
+    end
+    g.wait
+  end
+end
---
+++ test/stackexchange/test_stackexchange.rb
@@ -0,0 +1,16 @@
+
+require 'rest-more/test'
+
+describe RC::StackExchange do
+  after do
+    WebMock.reset!
+  end
+
+  should 'me' do
+    stub_request(:get,
+      'https://api.stackexchange.com/me?key=yek&site=stackoverflow').
+      to_return(:body => '{"name":"meme"}')
+
+    RC::StackExchange.new(:key => 'yek').me.should.eq 'name' => 'meme'
+  end
+end
